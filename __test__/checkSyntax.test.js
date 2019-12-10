880455e68e7a9c87278ca321668701ed8cb1bef6
Mike Avdeev <avdeev2033@icloud.com>
2019-12-10 16:39:20 +0300

    fix test

--- __test__/checkSyntax.test.js
+++ __test__/checkSyntax.test.js
@@ -1,1 +1,29 @@
-import checkSyntax from '../src/checkSyntax';test('should be balanced', () => {  const str1 = '---(++++)----';  expect(checkSyntax(str1)).toEqual(0);  const str2 = '';  expect(checkSyntax(str2)).toEqual(0);  const str3 = 'before ( middle []) after';  expect(checkSyntax(str3)).toEqual(0);  const str4 = '(  [  <>  ()  ]  <>  )';  expect(checkSyntax(str4)).toEqual(0);});test('should not be balanced', () => {  const str1 = ') (';  expect(checkSyntax(str1)).toEqual(1);  const str2 = '} {';  expect(checkSyntax(str2)).toEqual(1);  const str3 = '<(   >)';  expect(checkSyntax(str3 )).toEqual(1);  const str4 = '   (      [)';  expect(checkSyntax(str4)).toEqual(1);});
+import checkSyntax from '../src/checkSyntax';
+
+test('should be balanced', () => {
+  const str1 = '---(++++)----';
+  expect(checkSyntax({ str: str1 })).toEqual(0);
+
+  const str2 = '';
+  expect(checkSyntax({ str: str2 })).toEqual(0);
+
+  const str3 = 'before ( middle []) after';
+  expect(checkSyntax({ str: str3 })).toEqual(0);
+
+  const str4 = '(  [  <>  ()  ]  <>  )';
+  expect(checkSyntax({ str: str4 })).toEqual(0);
+});
+
+test('should not be balanced', () => {
+  const str1 = ') (';
+  expect(checkSyntax({ str: str1 })).toEqual(1);
+
+  const str2 = '} {';
+  expect(checkSyntax({ str: str2 })).toEqual(1);
+
+  const str3 = '<(   >)';
+  expect(checkSyntax({ str: str3 })).toEqual(1);
+
+  const str4 = '   (      [)';
+  expect(checkSyntax({ str: str4 })).toEqual(1);
+});
